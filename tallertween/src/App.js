6ea3c9f76746b42e6947b791ddf8c0da828460cc
Bsmo <bmartinezo@ucentral.edu.co>
2023-03-24 13:41:23 -0500

    Se comenta la barra fps

--- tallertween/src/App.js
+++ tallertween/src/App.js
@@ -10,24 +10,24 @@ function App() {
     test.initialize();
     test.animate();
 
-    // part 0 - add axis helper
-    const axesHelper = new THREE.AxesHelper(1);
-    test.scene.add(axesHelper);
 
-    // part 0 - add box mesh which will be tweened
+    /*  const axesHelper = new THREE.AxesHelper(1);
+     test.scene.add(axesHelper); */
+
+
     const boxGeometry = new THREE.BoxGeometry(4, 4, 4);
     const boxMaterial = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     test.scene.add(boxMesh);
 
-    // part 0 - add ground mesh for reference
+
     /*  const groundGeometry = new THREE.BoxGeometry(24, 1, 24);
      const groundMaterial = new THREE.MeshNormalMaterial();
      const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
      groundMesh.position.y = -4;
      test.scene.add(groundMesh); */
 
-    // part 0 - ensure that tween.js is running
+
     const animate = (t) => {
       TWEEN.update(t);
       window.requestAnimationFrame(animate);
@@ -35,7 +35,7 @@ function App() {
     animate();
 
 
-    // part 3 - chaining tweens together
+
     const tween1 = new TWEEN.Tween({ x: 0, y: 0, xRotation: 0 })
       .to({ x: 5, y: 8, xRotation: Math.PI / 2 }, 2000)
       .onUpdate((coords) => {
@@ -45,6 +45,7 @@ function App() {
       })
       .easing(TWEEN.Easing.Exponential.InOut)
       .delay(100);
+
     const tween2 = new TWEEN.Tween({ x: 5, y: 8, xRotation: Math.PI / 2 })
       .to({ x: 0, y: 0, xRotation: 0 }, 2000)
       .onUpdate((coords) => {
--- tallertween/src/components/Model.js
+++ tallertween/src/components/Model.js
@@ -4,23 +4,22 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 
 export default class SceneInit {
     constructor(canvasId) {
-        // NOTE: Core components to initialize Three.js app.
+
         this.scene = undefined;
         this.camera = undefined;
         this.renderer = undefined;
 
-        // NOTE: Camera params;
+
         this.fov = 45;
         this.nearPlane = 1;
         this.farPlane = 1000;
         this.canvasId = canvasId;
 
-        // NOTE: Additional components.
         this.clock = undefined;
         this.stats = undefined;
         this.controls = undefined;
 
-        // NOTE: Lighting is basically required.
+
         this.ambientLight = undefined;
         this.directionalLight = undefined;
     }
@@ -36,50 +35,51 @@ export default class SceneInit {
         this.camera.position.z = 36;
         this.camera.position.y = 12;
 
-        // NOTE: Specify a canvas which is already created in the HTML.
         const canvas = document.getElementById(this.canvasId);
         this.renderer = new THREE.WebGLRenderer({
             canvas,
 
         });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        // this.renderer.shadowMap.enabled = true;
+
         document.body.appendChild(this.renderer.domElement);
 
         this.clock = new THREE.Clock();
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.stats = Stats();
-        document.body.appendChild(this.stats.dom);
 
-        // ambient light which is for the whole scene
+        //CAJA RENDIEMIENTO TALLER
+        /* this.stats = Stats();
+        document.body.appendChild(this.stats.dom); */
+
+
         this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         this.ambientLight.castShadow = true;
         this.scene.add(this.ambientLight);
 
-        // directional light - parallel sun rays
+
         this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        // this.directionalLight.castShadow = true;
+
         this.directionalLight.position.set(0, 32, 64);
         this.scene.add(this.directionalLight);
 
-        // if window resizes
+
         window.addEventListener('resize', () => this.onWindowResize(), false);
 
 
     }
 
     animate() {
-        // NOTE: Window is implied.
-        // requestAnimationFrame(this.animate.bind(this));
+
         window.requestAnimationFrame(this.animate.bind(this));
         this.render();
-        this.stats.update();
+        //this.stats.update();
         this.controls.update();
     }
 
+
+
     render() {
-        // NOTE: Update uniform data on each render.
-        // this.uniforms.u_time.value += this.clock.getDelta();
+
         this.renderer.render(this.scene, this.camera);
     }
 
